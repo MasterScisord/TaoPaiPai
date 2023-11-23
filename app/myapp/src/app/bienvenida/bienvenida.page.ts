93d87671f9a6f151da5491bff6a4454e7925fcd6
unknown <pedroespinozazul@gmail.com>
2023-11-23 20:32:42 -0300

    implementacion camara

--- app/myapp/src/app/bienvenida/bienvenida.page.ts
+++ app/myapp/src/app/bienvenida/bienvenida.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular'; // Importa NavController
+import { Capacitor } from '@capacitor/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';  // Importa BarcodeScanner
+import { Plugins } from '@capacitor/core';
+
+import { NavigationExtras } from '@angular/router';
+const { TextToSpeech } = Plugins as any;
 
 @Component({
   selector: 'app-bienvenida',
@@ -15,6 +22,23 @@ export class BienvenidaPage implements OnInit {
     this.activatedRoute.paramMap.subscribe((params) => {
       this.username = params.get('username') ?? ''; // Asigna un valor predeterminado si es null
     });
+  }
+
+  async escanearQR() {
+    if (Capacitor.isPluginAvailable('BarcodeScanner')) {
+      try {
+        const result = await BarcodeScanner.startScan();
+        if (result.hasContent) {
+          // Maneja el resultado del escaneo como desees,
+          // en este caso, abre la URL escaneada en una nueva ventana.
+          window.open(result.content, '_blank');
+        }
+      } catch (error) {
+        console.error('Error al escanear:', error);
+      }
+    } else {
+      console.warn('La funcionalidad de escaneo de QR solo está disponible en dispositivos nativos.');
+    }
   }
 }
   
